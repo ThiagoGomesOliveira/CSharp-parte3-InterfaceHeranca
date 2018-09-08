cb8acfe2ad2f7e5834236f4fa504512a4b0c7c93
ThiagoGomesOliveira <thiago.blu1988@gmail.com>
2018-09-08 16:54:56 -0300

    Alura : Conhecendo o problema do cliente

---
+++ ByteBank/ByteBank/Program.cs
@@ -0,0 +1,32 @@
+﻿using ByteBank.Funcionarios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GerenciadorBonificacao gerenciador = new GerenciadorBonificacao();
+            Funcionario funcionario = new Funcionario();
+            funcionario.Nome = "Thiago Gomes Oliveira";
+            funcionario.Cpf = "xxx.xxx.xxx-xx";
+            funcionario.Salario = 2000;
+
+            Diretor diretor = new Diretor();
+            diretor.Nome = "Juliana";
+            diretor.Cpf = "xxx.xxx.xxx-xx";
+            diretor.Salario = 5000;
+
+            gerenciador.Registrar(funcionario);
+            gerenciador.Registrar(diretor);
+
+            Console.WriteLine("Total de bonificações: " + gerenciador.GetTotalBonificacao());
+            Console.ReadLine();
+        }
+    }
+}
